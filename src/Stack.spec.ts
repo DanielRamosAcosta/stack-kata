fa835ad98d823f69a2889cdd06e75833087730bd
Jordi Martí <jordimarti@multiplica.com>
2023-03-17 13:19:29 -0500

    pushes various elements & pops various elements

--- src/Stack.spec.ts
+++ src/Stack.spec.ts
@@ -21,6 +21,15 @@ describe("Stack", () => {
     expect(stack.peek()).toBe("foo")
   })
 
+  it("pushes various elements", () => {
+    const stack = new Stack()
+
+    stack.push("foo")
+    stack.push("bar")
+
+    expect(stack.size).toBe(2)
+  })
+
   it("peeks the last element", () => {
     const stack = new Stack()
     const element = "bar"
@@ -40,6 +49,20 @@ describe("Stack", () => {
 
     expect(poppedElement).toBe("foo")
     expect(stack.isEmpty()).toBe(true)
+  })
+
+  it("pops various elements", () => {
+    const stack = new Stack()
+    const element1 = "foo"
+    const element2 = "bar"
+    stack.push(element1)
+    stack.push(element2)
+
+    const poppedElement2 = stack.pop()
+    const poppedElement1 = stack.pop()
+
+    expect(poppedElement2).toBe(element2)
+    expect(poppedElement1).toBe(element1)
   })
 
   it("gets the size of the stack", () => {
